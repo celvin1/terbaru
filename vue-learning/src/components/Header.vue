278ef9083e5e1b4799c11c3814f100d5001d6cc0
davin <davin_257@smkassalaambandung.sch.id>
2023-08-04 15:08:59 +0700

    first commit

---
+++ vue-learning/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <div>
+            <h1>Ini Dari Header</h1>
+            <p>Halo {{ text }} ini dari Header</p>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: ['text'],
+}
+</script>
---
+++ vue-learning/src/components/MyComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Halo Dunia</h1>
+    <p> Nama :{{ nama }}</p>
+    <p> Umur :{{ umur }}</p>
+</template>
+
+<script>  
+export default {
+    props: ['nama', 'umur']
+}
+</script>
